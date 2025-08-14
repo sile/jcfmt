da682b30abd193818674a9ebc51434acd9658294
Takeru Ohta <phjgt308@gmail.com>
2025-08-15 08:31:12 +0900

    Replace simple "Hello, world!" with command-line argument parsing using noargs crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+fn main() -> noargs::Result<()> {
+    let mut args = noargs::raw_args();
+
+    args.metadata_mut().app_name = env!("CARGO_PKG_NAME");
+    args.metadata_mut().app_description = env!("CARGO_PKG_DESCRIPTION");
+
+    if noargs::VERSION_FLAG.take(&mut args).is_present() {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+    noargs::HELP_FLAG.take_help(&mut args);
+
+    let mut input_files = Vec::new();
+    while let Some(a) = noargs::arg("[INPUT_FILE]...").take(&mut args).present() {
+        input_files.push(PathBuf::from(a.value()));
+    }
+
+    if let Some(help) = args.finish()? {
+        print!("{help}");
+        return Ok(());
+    }
+
+    Ok(())
 }
